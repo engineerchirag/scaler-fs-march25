17c8026b5e59813589d5989175597006d1548b28
Chirag Goel <engineerchirag021@gmail.com>
2025-06-13 09:04:03 +0530

    React Class1

---
+++ Frontend/React/Class1/my-app/src/main.jsx
@@ -0,0 +1,46 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+function Heading (props) {
+  const { title } = props;
+  console.log(props.title);
+  // let name = "Chirag";
+  return (
+    // <h1>Hello World! {name}</h1>
+    <h1>Hello World! {title}</h1>
+  )
+}
+
+const List = () => {
+  return (
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+      </ul>
+  )
+}
+
+const MyApp = () => {
+  return (
+    <div>
+      <Heading title="Chirag" />
+      <Heading title="Nithish" />
+      <Heading title="Nikhilesh" />
+      <List />
+    </div>
+  )
+}
+
+createRoot(document.getElementById('root')).render(
+  <MyApp />
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+)
